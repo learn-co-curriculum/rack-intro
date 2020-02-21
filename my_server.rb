54af619a9178bf6c8b4b2dbb4c8e1fd274d8161c
maxwellbenton <maxwellbenton@gmail.com>
2020-02-21 09:13:02 -0500

    rebase, update solution

--- my_server.rb
+++ my_server.rb
@@ -1,6 +1,3 @@
-require 'rack'
-
-# Something that responds to call, that's what Rack demands
 class MyServer
   def call(env)
     return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
@@ -9,6 +6,4 @@ class MyServer
   def pretty_response
     (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
-end
-
-run MyServer.new
+end 
